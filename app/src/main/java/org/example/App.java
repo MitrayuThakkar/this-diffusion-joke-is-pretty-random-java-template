704809403f5af5852565813494df7dab3618dbed
Mitrayu Thakkar <tm85091@mail.harpercollege.edu>
2025-05-27 18:06:25 -0500

    This diffusion joke pretty Random
    output of This diffusion joke pretty Random

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,25 @@
 package org.example;
+import java.util.Random;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        int[] stepOptions = {10, 20, 50, 100, 200}; // Values of N
+        int trials = 10; // M value
+        Random rand = new Random();
+
+        System.out.printf("%-20s %-20s %-20s%n", "Number of Steps (N)", "Trial Number (M)", "Final Position (D)");
+
+        for (int N : stepOptions) {
+            for (int trial = 1; trial <= trials; trial++) {
+                int currentPosition = 0;
+
+                for (int step = 0; step < N; step++) {
+                    currentPosition += rand.nextBoolean() ? 1 : -1;
+                }
+
+                System.out.printf("%-20d %-20d %-20d%n", N, trial, currentPosition);
+            }
+        }
     }
 }
--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -1,11 +1,27 @@
 package org.example;
-
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.Random; // <-- Add this line
 
-class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+public class AppTest {
+
+    @Test
+    public void testRandomWalkWithinReasonableBounds() {
+        int N = 100;
+        int trials = 10;
+
+        for (int i = 0; i < trials; i++) {
+            int position = runSingleWalk(N);
+            assertTrue(Math.abs(position) <= N, "Position should not exceed N steps in either direction");
+        }
+    }
+
+    private int runSingleWalk(int N) {
+        int pos = 0;
+        Random rand = new Random();
+        for (int i = 0; i < N; i++) {
+            pos += rand.nextBoolean() ? 1 : -1;
+        }
+        return pos;
     }
 }
